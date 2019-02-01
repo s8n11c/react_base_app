aedc9d7224650e1944633ed2bdd162d7e928343d
crap <s8n11c@gmail.com>
2019-02-01 14:58:33 -0500

    countdown

--- src/app/countdown/components/index.js
+++ src/app/countdown/components/index.js
@@ -5,32 +5,43 @@ class CountDown extends React.Component {
   constructor() {
      super();
      this.state={years: "0", month: "0", day: "0", hours: "0", min: "0", seconds: "0"};
-     //until 1-3-2019
-     this.until={years: 2019, month: "3" , day: "1" , hours: "0", min: "0" , seconds: "0"};
+
+     this.until={years: 2019, month: 3 , day: 1 , hours: 0, min: 0 , seconds: 0};
      this.updateStateToDate=this.updateStateToDate.bind(this);
    }
 
    updateStateToDate(){
-      let currentDate=Date.now();
-      console.log(currentDate);
+      const  oneDay = 24*60*60*1000;
+      let currentDate=new Date();
+      let untilDate=new Date(Date.UTC(this.until.years,this.until.month-1,this.until.day,this.until.hours,this.until.min,this.until.seconds));
+      let durationDate=Math.round(Math.abs((currentDate.getTime() - untilDate.getTime())/(oneDay)))
+      let months=Math.floor(durationDate/30)
+      let years=Math.floor(durationDate/365)
+      this.setState({years: years ,
+                    month: months ,
+                    day:    durationDate%30 ,
+                    hours: Math.abs(24-currentDate.getHours()) ,
+                    min: Math.abs(60-currentDate.getMinutes()) ,
+                    seconds: Math.abs(60-currentDate.getSeconds()) });
    }
 
-
   render() {
-    console.log("current state"+this.state);
     return <div className='container'>
-              <h1> Count Down </h1>
+              <h1>|| Count Down ||</h1>
 
-              <a> {this.state.currentTime}</a>
+              <h6> {this.state.years}/{this.state.month}/{this.state.day}   --- {this.state.hours}:{this.state.min}:{this.state.seconds}</h6>
           </div>;
   }
   componentDidMount(){
-    this.interval = setInterval(() => this.updateStateToDate, 1000);
+    this.interval = setInterval(this.updateStateToDate, 1000);
   }
   componentWillUnmount() {
    clearInterval(this.interval);
 }
+
+
 }
+
 
 
 export default CountDown;
